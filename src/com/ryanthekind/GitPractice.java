02ab28b6d8ae65e8d8513d4b0a7f59fc70a29ed5
Ryan Burns <mr.ryan.burns@outlook.com>
2022-02-12 17:17:52 -0500

    added changes to my original code

---
+++ src/com/ryanthekind/GitPractice.java
@@ -0,0 +1,19 @@
+package com.ryanthekind;
+
+public class GitPractice {
+
+	/*
+	 * This class was created to practice with 
+	 * using git on the command line
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("I am making changes to my code!");
+		
+		for(int count = 0; count <= 5; count++) {
+			System.out.println(count);
+		}
+		
+	}
+
+}
